34cb495d63cd61ab46833abfd59646d950253900
Jessica Bareiro Aponte <jebareiro@dncp.gov.py>
2025-08-07 12:33:56 -0300

    Finalizacion y agregacion de funciones y comentarios

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-let amigos = [];
+let amigos = []; // Array donde se guardan los nombres
 
 function agregarAmigo() {
     let insercion = document.getElementById('amigo');
-    let nombre = insercion.value;
+    let nombre = insercion.value; // Obtener el valor escrito
 
+    // Validar que no este vacio
     if (nombre === '') {
         alert('Por favor, inserte un nombre.');
         return;
     }
-    amigos.push(nombre);
-    agregarAmigo.value = '';
+    amigos.push(nombre); // Agregar el nombre al array
+    insercion.value = ''; // Limpia el input
     actualizarLista();
 }
 
@@ -17,8 +18,17 @@ function actualizarLista() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
     
+    // Recorrer el array
     for (let i = 0; i < amigos.length; i++){
-        let amigo = amigos[i];
-        lista.innerHTML += amigos[i];
+        lista.innerHTML += '<li>' + amigos[i] + '</li>';
     }
+}
+
+function sortearAmigo() {
+    // Genera indice aleatorio
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indice]; // Obtener nombre sorteado
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = amigoSorteado; // Muestra el nombre sorteado
 }
