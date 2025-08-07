06af1e122bb9f292ed706f0e0298fc8805b06a88
Jessica Bareiro Aponte <jebareiro@dncp.gov.py>
2025-08-07 12:23:21 -0300

    Agregacion de funciones

--- app.js
+++ app.js
@@ -10,4 +10,15 @@ function agregarAmigo() {
     }
     amigos.push(nombre);
     agregarAmigo.value = '';
+    actualizarLista();
+}
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++){
+        let amigo = amigos[i];
+        lista.innerHTML += amigos[i];
+    }
 }
